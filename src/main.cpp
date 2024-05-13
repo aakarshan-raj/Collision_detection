b4b66de2436e395cf6cc38a7070856fac22d0621
aakarshan-raj <aakarshan7raj@gmail.com>
2024-05-13 20:27:56 +0530

    collision resolution

--- src/main.cpp
+++ src/main.cpp
@@ -390,38 +390,76 @@ int main()
 
                 if (prev_overlap.first > 0) // TOP-DOWN collision
                 {
-                    std::cout << "Collision origin: vertical, magnitude:" << prev_overlap.first << ":" << prev_overlap.second << std::endl; // TOP collision
                     if (draw_rect1)
                     {
-                        shape1.rect.setPosition(sf::Vector2f(shape1.rect.getPosition().x, shape1.rect.getPosition().y - check_collision.second));
-                        shape1.bounding_box.setPosition(sf::Vector2f(shape1.rect.getPosition().x, shape1.rect.getPosition().y));
-                        shape1.X.setPosition(sf::Vector2f(shape1.X.getPosition().x, shape1.rect.getPosition().y));
-                        shape1.Y.setPosition(sf::Vector2f(shape1.Y.getPosition().x, shape1.rect.getPosition().y));
+                        if (shape1.rect.getPosition().y > shape2.rect.getPosition().y) // TOP collision
+                        {
+                            shape1.rect.setPosition(sf::Vector2f(shape1.rect.getPosition().x, shape1.rect.getPosition().y + check_collision.second));
+                            shape1.bounding_box.setPosition(sf::Vector2f(shape1.rect.getPosition().x, shape1.rect.getPosition().y));
+                            shape1.X.setPosition(sf::Vector2f(shape1.X.getPosition().x, shape1.rect.getPosition().y));
+                            shape1.Y.setPosition(sf::Vector2f(shape1.Y.getPosition().x, shape1.rect.getPosition().y));
+                        }
+                        else // DOWN collision
+                        {
+                            shape1.rect.setPosition(sf::Vector2f(shape1.rect.getPosition().x, shape1.rect.getPosition().y - check_collision.second));
+                            shape1.bounding_box.setPosition(sf::Vector2f(shape1.rect.getPosition().x, shape1.rect.getPosition().y));
+                            shape1.X.setPosition(sf::Vector2f(shape1.X.getPosition().x, shape1.rect.getPosition().y));
+                            shape1.Y.setPosition(sf::Vector2f(shape1.Y.getPosition().x, shape1.rect.getPosition().y));
+                        }
                     }
                     else if (draw_rect2)
                     {
-                        shape2.rect.setPosition(sf::Vector2f(shape2.rect.getPosition().x, shape2.rect.getPosition().y - check_collision.second));
-                        shape2.bounding_box.setPosition(sf::Vector2f(shape2.rect.getPosition().x, shape2.rect.getPosition().y));
-                        shape2.X.setPosition(sf::Vector2f(shape2.X.getPosition().x, shape2.rect.getPosition().y));
-                        shape2.Y.setPosition(sf::Vector2f(shape2.Y.getPosition().x, shape2.rect.getPosition().y));
+                        if (shape2.rect.getPosition().y > shape1.rect.getPosition().y) // TOP
+                        {
+                            shape2.rect.setPosition(sf::Vector2f(shape2.rect.getPosition().x, shape2.rect.getPosition().y + check_collision.second));
+                            shape2.bounding_box.setPosition(sf::Vector2f(shape2.rect.getPosition().x, shape2.rect.getPosition().y));
+                            shape2.X.setPosition(sf::Vector2f(shape2.X.getPosition().x, shape2.rect.getPosition().y));
+                            shape2.Y.setPosition(sf::Vector2f(shape2.Y.getPosition().x, shape2.rect.getPosition().y));
+                        }
+                        else // DOWN
+                        {
+                            shape2.rect.setPosition(sf::Vector2f(shape2.rect.getPosition().x, shape2.rect.getPosition().y - check_collision.second));
+                            shape2.bounding_box.setPosition(sf::Vector2f(shape2.rect.getPosition().x, shape2.rect.getPosition().y));
+                            shape2.X.setPosition(sf::Vector2f(shape2.X.getPosition().x, shape2.rect.getPosition().y));
+                            shape2.Y.setPosition(sf::Vector2f(shape2.Y.getPosition().x, shape2.rect.getPosition().y));
+                        }
                     }
                 }
                 else if (prev_overlap.second > 0) // RIGHT LEFT COLLISION
                 {
-                    std::cout << "Collision origin: horizontal, magnitude:" << prev_overlap.second << ":" << prev_overlap.first << std::endl;
-                    if (draw_rect1) // RIGHT collision
+                    if (draw_rect1)
                     {
-                        shape1.rect.setPosition(sf::Vector2f(shape1.rect.getPosition().x + check_collision.first, shape1.rect.getPosition().y));
-                        shape1.bounding_box.setPosition(sf::Vector2f(shape1.rect.getPosition().x, shape1.rect.getPosition().y));
-                        shape1.X.setPosition(sf::Vector2f(shape1.rect.getPosition().x - 25, shape1.X.getPosition().y));
-                        shape1.Y.setPosition(sf::Vector2f(shape1.rect.getPosition().x + 10, shape1.Y.getPosition().y));
+                        if (shape1.rect.getPosition().x > shape2.rect.getPosition().x) // RIGHT collision
+                        {
+                            shape1.rect.setPosition(sf::Vector2f(shape1.rect.getPosition().x + check_collision.first, shape1.rect.getPosition().y));
+                            shape1.bounding_box.setPosition(sf::Vector2f(shape1.rect.getPosition().x, shape1.rect.getPosition().y));
+                            shape1.X.setPosition(sf::Vector2f(shape1.rect.getPosition().x - 25, shape1.X.getPosition().y));
+                            shape1.Y.setPosition(sf::Vector2f(shape1.rect.getPosition().x + 10, shape1.Y.getPosition().y));
+                        }
+                        else // LEFT
+                        {
+                            shape1.rect.setPosition(sf::Vector2f(shape1.rect.getPosition().x - check_collision.first, shape1.rect.getPosition().y));
+                            shape1.bounding_box.setPosition(sf::Vector2f(shape1.rect.getPosition().x, shape1.rect.getPosition().y));
+                            shape1.X.setPosition(sf::Vector2f(shape1.rect.getPosition().x - 25, shape1.X.getPosition().y));
+                            shape1.Y.setPosition(sf::Vector2f(shape1.rect.getPosition().x + 10, shape1.Y.getPosition().y));
+                        }
                     }
                     else if (draw_rect2)
                     {
-                        shape2.rect.setPosition(sf::Vector2f(shape2.rect.getPosition().x + check_collision.first, shape2.rect.getPosition().y));
-                        shape2.bounding_box.setPosition(sf::Vector2f(shape2.rect.getPosition().x, shape2.rect.getPosition().y));
-                        shape2.X.setPosition(sf::Vector2f(shape2.rect.getPosition().x - 25, shape2.X.getPosition().y));
-                        shape2.Y.setPosition(sf::Vector2f(shape2.rect.getPosition().x + 10, shape2.Y.getPosition().y));
+                        if (shape2.rect.getPosition().x > shape1.rect.getPosition().x) // RIGHT
+                        {
+                            shape2.rect.setPosition(sf::Vector2f(shape2.rect.getPosition().x + check_collision.first, shape2.rect.getPosition().y));
+                            shape2.bounding_box.setPosition(sf::Vector2f(shape2.rect.getPosition().x, shape2.rect.getPosition().y));
+                            shape2.X.setPosition(sf::Vector2f(shape2.rect.getPosition().x - 25, shape2.X.getPosition().y));
+                            shape2.Y.setPosition(sf::Vector2f(shape2.rect.getPosition().x + 10, shape2.Y.getPosition().y));
+                        }
+                        else // LEFT
+                        {
+                            shape2.rect.setPosition(sf::Vector2f(shape2.rect.getPosition().x - check_collision.first, shape2.rect.getPosition().y));
+                            shape2.bounding_box.setPosition(sf::Vector2f(shape2.rect.getPosition().x, shape2.rect.getPosition().y));
+                            shape2.X.setPosition(sf::Vector2f(shape2.rect.getPosition().x - 25, shape2.X.getPosition().y));
+                            shape2.Y.setPosition(sf::Vector2f(shape2.rect.getPosition().x + 10, shape2.Y.getPosition().y));
+                        }
                     }
                 }
 
